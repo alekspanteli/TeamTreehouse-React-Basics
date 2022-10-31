d1e34dae1c40f65d19de427b5351037dacd6c7fd
alekspanteli <alekspanteli@rambler.ru>
2022-10-31 13:20:32 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,35 @@ import reactLogo from "./assets/react.svg";
 
 const players = [
   {
+    id: "1",
     name: "Guil",
     score: 50,
   },
   {
+    id: "2",
     name: "Treasure",
     score: 85,
   },
   {
+    id: "3",
     name: "Ashley",
     score: 95,
   },
   {
+    id: "4",
     name: "James",
     score: 80,
   },
 ];
 
-const Header = (prosp) => {
+const Header = (props) => {
   return (
     <header className="px-7 py-8 bg-[#8063FA] rounded-t-md flex items-center border-solid border-b-1 border-[#D4CAFF] text-white gap-5">
       <h1 className="grow font-normal text-2xl text-right tracking-wider">
-        {prosp.title}
+        {props.title}
       </h1>
       <span className="text-sm font-semibold">
-        Players: {prosp.totalPlayers}
+        Players: {props.players.length}
       </span>
     </header>
   );
@@ -34,14 +38,17 @@ const Header = (prosp) => {
 
 const Player = (props) => {
   return (
-    <div>
-      {props.initialPlayers.map((player) => (
-        <div className="flex justify-between items-center bg-white">
+    <>
+      {props.players.map((player) => (
+        <div
+          key={player.id}
+          className="flex justify-between items-center bg-white"
+        >
           <span className="text-2xl font-bold pl-7">{player.name}</span>
           <Counter score={player.score} />
         </div>
       ))}
-    </div>
+    </>
   );
 };
 
@@ -73,9 +80,9 @@ const Counter = (props) => {
 function App() {
   return (
     <div className="container mx-auto rounded-b-md border-solid border-b-4 border-[#D4CAFF]">
-      <Header title="scoreboard" totalPlayers={2} />
+      <Header title="scoreboard" players={players} />
       <main className="divide-y divide-slate-200">
-        <Player initialPlayers={players} />
+        <Player players={players} />
       </main>
     </div>
   );
