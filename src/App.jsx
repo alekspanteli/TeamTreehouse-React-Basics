f01dda0ec6432605ba46ba3bbe7ea571539ee759
alekspanteli <alekspanteli@rambler.ru>
2022-10-31 10:02:54 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,41 @@ const Header = () => {
       <h1 className="grow font-normal text-2xl text-right tracking-wider">
         Scoreboard
       </h1>
-      <span className="text-sm">Players: 1</span>
+      <span className="text-sm font-semibold">Players: 1</span>
     </header>
+  );
+};
+
+const Player = () => {
+  return (
+    <div>
+      <Header />
+      <div className="flex justify-between items-center bg-white rounded-b-md border-solid border-b-4 border-[#D4CAFF]">
+        <span className="text-2xl font-bold pl-7">Alex</span>
+        <Counter />
+      </div>
+    </div>
+  );
+};
+
+const Counter = () => {
+  return (
+    <div className="flex gap-3 items-center">
+      <button className="text-slate-900 text-3xl p-5 bg-slate-200 hover:bg-slate-300">
+        -
+      </button>
+      <span className="font-bold text-xl">35</span>
+      <button className="text-slate-900 text-3xl p-5 bg-slate-200 hover:bg-slate-300">
+        +
+      </button>
+    </div>
   );
 };
 
 function App() {
   return (
     <div className="container mx-auto">
-      <Header />
+      <Player />
     </div>
   );
 }
