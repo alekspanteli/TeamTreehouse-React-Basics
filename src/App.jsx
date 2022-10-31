aacb84a41a6a61e3d84f28dc32169cd328232a6b
alekspanteli <alekspanteli@rambler.ru>
2022-10-31 13:48:14 +0000

    Update 2 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,6 @@
 import reactLogo from "./assets/react.svg";
 
-const players = [
-  {
-    id: "1",
-    name: "Guil",
-    score: 50,
-  },
-  {
-    id: "2",
-    name: "Treasure",
-    score: 85,
-  },
-  {
-    id: "3",
-    name: "Ashley",
-    score: 95,
-  },
-  {
-    id: "4",
-    name: "James",
-    score: 80,
-  },
-];
+import players from "./data/data";
 
 const Header = (props) => {
   return (
@@ -29,9 +8,7 @@ const Header = (props) => {
       <h1 className="grow font-normal text-2xl text-right tracking-wider">
         {props.title}
       </h1>
-      <span className="text-sm font-semibold">
-        Players: {props.players.length}
-      </span>
+      <span className="text-sm font-semibold">Players: {players.length}</span>
     </header>
   );
 };
@@ -39,15 +16,16 @@ const Header = (props) => {
 const Player = (props) => {
   return (
     <>
-      {props.players.map((player) => (
-        <div
-          key={player.id}
-          className="flex justify-between items-center bg-white"
-        >
-          <span className="text-2xl font-bold pl-7">{player.name}</span>
-          <Counter score={player.score} />
-        </div>
-      ))}
+      {props.initialPlayers &&
+        props.initialPlayers.map((player) => (
+          <div
+            key={player.id}
+            className="flex justify-between items-center bg-white"
+          >
+            <span className="text-2xl font-bold pl-7">{player.name}</span>
+            <Counter score={player.score} />
+          </div>
+        ))}
     </>
   );
 };
@@ -80,9 +58,9 @@ const Counter = (props) => {
 function App() {
   return (
     <div className="container mx-auto rounded-b-md border-solid border-b-4 border-[#D4CAFF]">
-      <Header title="scoreboard" players={players} />
+      <Header title="scoreboard" />
       <main className="divide-y divide-slate-200">
-        <Player players={players} />
+        <Player initialPlayers={players} />
       </main>
     </div>
   );
